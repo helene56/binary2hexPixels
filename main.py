ce80844e1748aab44653a2f3a86ec3eb7ba8d98b
helene <helene.maja@hotmail.dk>
2024-05-24 11:17:48 +0200

    adjusted main.py file structure and add add_padding function

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+# pixels wide - needed variable for adjusting padding
+PIXELS_WIDE = 17
 
-# should add something that determines the range, now it is set to fit a binary number that is 24 bit
+# file names
+binary_file = "test.txt"
+hex_file = "new.txt"
+# paths
+BIN_FILE_PATH = f"./binary_files/{binary_file}"
+HEX_FILE_PATH = "./hex_files/"
+
+# empty holder for the binary string
+binary_string = ""
+
+# empty holder for binary list
+binary_string_list_1 = []
+
 # Functions
 def turn_8bit(binary_string, binary_string_list):
     """Takes a binary_string and slices it into 8 characters at a time, and then appends it to binary_string_list.
@@ -16,26 +30,30 @@ def bit_to_hex(binary_integer_list):
     hexadecimal_list = [f"0x{num:02X}" for num in binary_integer_list]
     return hexadecimal_list
 
-# paths
-BIN_FILE_PATH = "./binary_files/test.txt"
-HEX_FILE_PATH = "./hex_files/"
-# empty holder for the binary string
-binary_string = ""
-# empty holder for binary list
-binary_string_list_1 = []
+def add_padding(pixel_width):
+    if pixel_width % 8 != 0:
+        padding = ""
+        needed_padding = (8 - (pixel_width % 8))
+        for _ in range(needed_padding):
+            padding += "0"
+        return padding
+
 # read binary file and write to hex file
 with open(BIN_FILE_PATH, 'r') as file:
-    with open(f"{HEX_FILE_PATH}/new.txt", 'w') as file_2:
+    with open(f"{HEX_FILE_PATH}/{hex_file}", 'w') as file_2:
         for line in file:
             for num in line:
                 if num == ',':
-                    binary_string += "0000000"
+                    padding = add_padding(PIXELS_WIDE)
+                    if padding:
+                        binary_string += padding
                     binary_int_list = turn_8bit(binary_string, binary_string_list_1)
                     hex_list = bit_to_hex(binary_int_list)
                     for value in hex_list:
                         file_2.write(f"{value} ")
                     file_2.write("\n")
                     print(hex_list)
+                    # reinitilaze as empty holders for the next line in file
                     binary_string = ""
                     binary_string_list_1 = []
                     continue
